ed9027b84517a0cee1794f94f65fdf3a6c53ce5e
vguilherme <vguilherme@uolinc.com>
2025-07-16 17:50:13 -0300

    feat: add date filter to payments-summary endpoint

--- src/main/kotlin/com/vitor/gui/rinhakotlinspring/Controller.kt
+++ src/main/kotlin/com/vitor/gui/rinhakotlinspring/Controller.kt
@@ -5,16 +5,18 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestClient
 import java.io.Serializable
+import java.time.Instant
+import java.time.OffsetDateTime
 
 @RestController
 class Controller(
-    private val restClient: RestClient,
-    private val redsQueueTemplate: RedisTemplate<String, QueueItem>,
-    private val redsPaymentItemTemplate: RedisTemplate<String, List<PaymentItem>>
+    private val redisQueueTemplate: RedisTemplate<String, QueueItem>,
+    private val redisPaymentItemTemplate: RedisTemplate<String, List<PaymentItem>>
 ) {
     companion object{
         private const val QUEUE_KEY: String = "PAYMENT_QUEUE"
@@ -31,28 +33,33 @@ class Controller(
             correlationId = input.correlationId,
             requestedAt = System.currentTimeMillis()
         )
-        redsQueueTemplate.opsForList().rightPush(QUEUE_KEY, queueItem)
+        redisQueueTemplate.opsForList().rightPush(QUEUE_KEY, queueItem)
         return ""
     }
 
     @GetMapping("/payments-summary")
     @ResponseStatus(HttpStatus.OK)
-    fun getPayments(): OutputBody {
+    fun getPayments(
+        @RequestParam (required = false) from: Instant?,
+        @RequestParam (required = false) to: Instant?
+    ): OutputBody {
         var defaultPaymentsCount = 0L
         var fallbackPaymentsCount = 0L
 
         var defaultPaymentsAmount = 0.0
         var fallbackPaymentsAmount = 0.0
 
-        val payments = redsPaymentItemTemplate.opsForValue().get(PAYMENTS_KEY)
+        val payments = redisPaymentItemTemplate.opsForValue().get(PAYMENTS_KEY)
 
         payments?.forEach {
-            if(it.processor == 'D'){
-                defaultPaymentsAmount += it.amount
-                defaultPaymentsCount++
-            } else {
-                fallbackPaymentsAmount += it.amount
-                fallbackPaymentsCount++
+            if(it.requestedAt > (from?.toEpochMilli() ?: 0L) && it.requestedAt < (to?.toEpochMilli() ?: Long.MAX_VALUE)) {
+                if(it.processor == 'D'){
+                    defaultPaymentsAmount += it.amount
+                    defaultPaymentsCount++
+                } else {
+                    fallbackPaymentsAmount += it.amount
+                    fallbackPaymentsCount++
+                }
             }
         }
         return OutputBody(
